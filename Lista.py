c56401b3fdb2355e6c75fd980e5070fbbc407334
Josue <2995284560101@ingenieria.usac.edu.gt>
2023-02-09 14:27:32 -0600

    creación de objetos y creando metodo de agregar lista

---
+++ Lista.py
@@ -0,0 +1,57 @@
+from nodo import Nodo
+class Lista:
+    def __init__(self):
+        self. first = None
+        self. final = None
+
+    def addFinalNode(self, dato):
+        newNodo = Nodo(dato) # se crea un nuevo nodo
+        #si la lista está vacia
+        if self.first == None: #si la lista no tiene ningun dato
+            self.first = newNodo #el apuntador primero apunta al nuevo nodo
+            self.final = newNodo #el apuntador ultimo apunta al nuevo nodo
+        else: #si la lista ya tiene uno o mas datos se agrega el nuevo nodo
+            self.final.after = newNodo #el apuntador siguiente apunta al nuevo nodo --->
+            newNodo.before = self.final #el apuntador anterior apunta al nodo anterior  <---
+            self.final = newNodo #el apuntador ultimo apunta al nuevo nodo
+
+    def printList(self):
+        count = 0
+        nodoTemporal = Nodo("")
+        nodoTemporal = self.first
+        while nodoTemporal != None:
+            count +=1
+            print("Nodo no. "+ str(count)+"valor: "+ nodoTemporal.dato)
+            nodoTemporal = nodoTemporal.after #pasa al siguiente nodo de la lista
+
+'''
+from NodoCurso import Curso
+
+class ListaDoble():
+    def __init__(self):
+        self.primero = None
+        self.ultimo = None
+        self.size = 0
+
+    def insertarCursoAlFinal(self, cod, nombre):
+        nuevo_curso = Curso(cod, nombre) # primer paso
+        self.size += 1
+        if self.primero is None:
+            self.primero = nuevo_curso
+            self.ultimo = nuevo_curso 
+        else: 
+            self.ultimo.setSiguiente(nuevo_curso)
+            nuevo_curso.setAnterior(self.ultimo)
+            self.ultimo = nuevo_curso
+
+    def mostrarCursos(self):
+        tmp = self.primero
+        for i in range(self.size):
+            print('Cod', tmp.cod, 'Nombre:',tmp.nombre)
+            tmp = tmp.getSiguiente()
+
+Lista_doble = ListaDoble()
+Lista_doble.insertarCursoAlFinal(6,"nombre1")
+Lista_doble.insertarCursoAlFinal(8,"Nombre2")
+Lista_doble.mostrarCursos()
+'''
--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-#Vamos a comenzar
+from xml.dom import minidom
+
+# leer archivo xml
+def cargarArchivo():
+    print('Ingrese la url del archivo:')
+    urlarchivo = input()
+    documento = minidom.parse(urlarchivo)
+    organismo = documento.getElementsByTagName("codigo")[0]
+    print(organismo.firstChild.data)
+
+'''
+Los datos iniciales serán cargados en un archivo XML, este archivo contendrá el listado de 
+los organismos que podrán ser incluidos en las muestras.  Por cada muestra se indicará la 
+cantidad M (filas) y la cantidad N (columnas), y luego se identificará cada celda que tenga un 
+organismo vivo y el nombre del organismo vivo que contiene.
+'''
+if __name__ == '__main__':
+    # menu    '''
+# '''
+    while True:
+        print("------- Menu Principal --------")
+        print("1. Cargar archivo xml")
+        # esto permite volver a cargar un archivo nuevo desde 0, pero no se deben perder los datos anterioresf
+        print("1. Reiniciar programa")
+        print("1. Guardar archivo xml")
+        print("2. Salir")
+        # recibe la opcion ingresada y la guarda como entero
+        opcion = int(input("Ingrese una opcion: "))
+        print()
+        if opcion == 1:
+            cargarArchivo()
+
+        elif opcion == 2:
+            break
+        else:
+            print("Ingrese una opcion correcta")
