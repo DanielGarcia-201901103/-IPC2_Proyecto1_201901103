2f6cbfedcc285ae638c967b9d75c34f54baba013
Daniel García <88566114+DanielGarcia-201901103@users.noreply.github.com>
2023-03-05 20:01:53 -0600

    Agregando diagramas a la documentación

--- Nodo.py
+++ Nodo.py
@@ -2,28 +2,4 @@ class Nodo:
     def __init__(self, dato):
         self.dato = dato # valor o objeto
         self.before = None #anterior
-        self.after = None  #siguiente
-
-'''
-class Curso():
-    def __init__(self, cod , nombre):
-        self.cod = cod
-        self.nombre = nombre
-        self.siguiente = None
-        self.anterior = None
-
-
-    def getSiguiente(self):
-        return self.siguiente
-
-    
-    def setSiguiente(self, curso):
-        self.siguiente = curso
-
-    def getAnterior(self):
-        return self.anterior
-
-    
-    def setAnterior(self, curso):
-        self.anterior = curso
-'''    
+        self.after = None  #siguiente  
